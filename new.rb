8758f5fc73d3d05f239105da3c780dd78c2a528f
EC2 Default User <yotchan32323@gmail.com>
2023-01-05 08:59:01 +0000

    chapter9-lesson

--- new.rb
+++ new.rb
@@ -17,9 +17,14 @@ car = Car.new
 car.move("右", 5)
 
 class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
   def self.run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
 
+Car.turn("右")
 Car.run(10)
